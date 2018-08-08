84fa2e2fd17346ea6cd640bd6e2d8ce40868a7c9
Alex <alexejmelnikov@rambler.ru>
2018-08-08 10:15:27 +0300

    Дз 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var budget = prompt("Ваш бюджет на месяц");
+var shop = prompt("Название вашеог магазина");
+var shopGoods=[], open, employers;
+employers={};
+
+mainList = {'budget': budget, "shop":shop, 'shopGoods':shopGoods, "employers":employers, "open":open};
+// ввод типов товара
+shopGoods[0] = prompt("Какой тип товаров будем продавать?");
+shopGoods[1] = prompt("Какой тип товаров будем продавать?");
+shopGoods[2] = prompt("Какой тип товаров будем продавать?");
+alert("Ваш бюджет на день: " + budget/30);
+
+console.log(mainList);

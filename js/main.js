16004a1ea659a1ca698a9419f4f05d912df33bb8
Alex <alexejmelnikov@rambler.ru>
2018-08-08 19:55:14 +0300

    Внёс коррективы

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var budget = prompt("Ваш бюджет на месяц");
 var shop = prompt("Название вашеог магазина");
-var shopGoods=[], open, employers;
+var shopGoods=[], open = true, employers, mainList;
 employers={};
 
-mainList = {'budget': budget, "shop":shop, 'shopGoods':shopGoods, "employers":employers, "open":open};
+mainList = {'budget': budget, "shop":shop, "shopGoods":shopGoods, "employers":employers. open};
 // ввод типов товара
 shopGoods[0] = prompt("Какой тип товаров будем продавать?");
 shopGoods[1] = prompt("Какой тип товаров будем продавать?");

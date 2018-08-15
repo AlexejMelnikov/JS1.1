298e4cd7b0dac106ef6217e3cc3beea29f951285
AlexejMelnikov <34479105+AlexejMelnikov@users.noreply.github.com>
2018-08-15 20:36:42 +0300

    Add files via upload

---
+++ lesson-05/2/js/script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelector("#open-btn");
+console.log(btn);
+
+let value = [];
+	value[0] = document.querySelectorAll('.name-value');
+	value[1] = document.querySelectorAll('.budget-value');
+    value[2] = document.querySelectorAll('.goods-value');
+	value[3] = document.querySelectorAll('.items-value');
+    value[4] = document.querySelectorAll('.employers-value');
+	value[5] = document.querySelectorAll('.discount-value');
+    value[6] = document.querySelectorAll('.isopen-value');
+console.log(value);
+
+let inputs =document.querySelectorAll('.goods-item');
+console.log(inputs)
+
+let tag =document.getElementsByTagName('button');
+console.log(tag);
+
+let budget = document.querySelector('.count-budget-value');
+console.log(budget);
+
+let time = document.querySelector('.time-value');
+console.log(time);
+
+let employers = document.querySelectorAll('.hire-employers-item');
+console.log(employers);

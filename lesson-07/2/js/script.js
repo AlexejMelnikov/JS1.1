aed4e92475d1e6e4fdca83be4d3afcea90bf254a
AlexejMelnikov <34479105+AlexejMelnikov@users.noreply.github.com>
2018-08-20 19:53:39 +0300

    Add files via upload

--- lesson-07/2/js/script.js
+++ lesson-07/2/js/script.js
@@ -1,1 +1,22 @@
-
+function startTime()
+{
+     let tm = new Date();
+     let h = tm.getHours();
+     let m = tm.getMinutes();
+     let s = tm.getSeconds();
+     h = checkTime(h);
+     m = checkTime(m);
+     s = checkTime(s);
+let timeNow = document.getElementById('time'); 
+timeNow.textContent = h + " : " + m + " : " + s;
+t = setTimeout(startTime, 1000);
+}
+		function checkTime(i)
+		{
+	if ( i < 10 ) {
+		i= "0" + i;
+		}
+		 return i;
+		}
+console.log(startTime());
+startTime();

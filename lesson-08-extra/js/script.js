874528d49dbd4c1bb0f08bcd6480f3292582be1d
AlexejMelnikov <34479105+AlexejMelnikov@users.noreply.github.com>
2018-08-23 23:36:32 +0300

    Add files via upload

--- lesson-08-extra/js/script.js
+++ lesson-08-extra/js/script.js
@@ -1,1 +1,50 @@
+let tab = document.querySelector('.tab'),
+tabs = document.querySelectorAll('.tabs-content'),
+container = document.querySelector('.container'),
+		height = 0,
+		id = 0,
+        // новая уставка положения монитора
+		heightNew = 0;
+		
+
+
+
+container.addEventListener('click', function(e) {
+	
+	
+			if(id < e.target.id) {
+				for(let i = 0;i < e.target.id; i++) {
+					heightNew = heightNew + tabs[i].scrollHeight;
+				}
+                console.log(heightNew);
+			} else {
+				for(let i = id;i > e.target.id ; i--) {
+					heightNew = heightNew - tabs[i].scrollHeight;
+				}
+				console.log(heightNew);
+			}
+			
+		        let heigth_timer_id = setInterval(function() {
+
+            if (height < heightNew) {
+                height ++;
+                scrollTo(0,height);
+                console.log(heightNew);
+               } 
+             if(height > heightNew) {
+               		console.log(heightNew);
+                	height--;
+					scrollTo(0, height);
+					}
+                
+              if(height == heightNew){
+                clearInterval(heigth_timer_id);    
+                id = e.target.id;
+                height = heightNew;
+                
+            }
+        }, 2);
+
+	
+});
 
